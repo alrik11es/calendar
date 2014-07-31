0c3ea34faaf1c8715ab4cf0b764b28798b892914
Marcos <alrik11es@hotmail.com>
2014-07-31 16:28:57 +0000

    Added multiple classes, basic configuration.

--- src/SSC/SSC.php
+++ src/SSC/SSC.php
@@ -1,5 +1,15 @@
 <?php
 
 class SSC{
+    
+    /** @var DateTime */
+    public $start_date;
+    /** @var DateInterval */
+    public $interval;
+    
+    public function getCalendar(FormatterInterface $format)
+    {
+        
+    }
 
 }
